fdbf08f9ce60831be94a83317d4d9bcc6f2613b5
MrChae1 <ljnellam27@gmail.com>
2023-10-29 15:19:01 +0700

    Add this js file for append modals

---
+++ src/modals.js
@@ -0,0 +1,13 @@
+import './scssFile/style.scss';
+
+// export const inputTag = (mainModals) => {
+//     mainModals.innerhtml = `
+//         <main class='tasksModalMain'>
+//             <section>
+//                 <input type='text'>
+//             </section>
+//         </main>
+//     `;
+//     document.body.appendChild(mainModals);
+//     document.body.style.overflow = "hidden";
+// }
